8548a81b35e4e33c2ac2b879afb6159b4dbf6ab4
Gwennael Gate <gwennaelgate@gmail.com>
2015-06-06 00:10:15 +0200

    printing full result in examples

--- faceexpressionestimation/faceexpressionestimation.py
+++ faceexpressionestimation/faceexpressionestimation.py
@@ -5,4 +5,4 @@ import angus
 conn = angus.connect()
 service = conn.services.get_service('face_expression_estimation', version=1)
 job = service.process({'image': open('./macgyver.jpg')})
-print job.result['faces']
+print job.result
--- motiondetection/motiondetection.py
+++ motiondetection/motiondetection.py
@@ -3,6 +3,6 @@
 import angus
 
 conn = angus.connect()
-service = conn.services.get_service('face_detection', version=1)
+service = conn.services.get_service('motion_detection', version=1)
 job = service.process({'image': open('./macgyver.jpg')})
-print job.result['faces']
+print job.result
