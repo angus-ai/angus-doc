fd60f6426cdcb7291bfef7053997008684ed10ff
Gwennael Gate <gwennaelgate@gmail.com>
2015-06-05 23:55:25 +0200

    printing full result in gaze_analysis example

--- gazeanalysis/gazeanalysis.py
+++ gazeanalysis/gazeanalysis.py
@@ -5,4 +5,4 @@ import angus
 conn = angus.connect()
 service = conn.services.get_service('gaze_analysis', version=1)
 job = service.process({'image': open('./macgyver.jpg')})
-print job.result['faces']
+print job.result
