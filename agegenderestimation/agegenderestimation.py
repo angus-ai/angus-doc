4c8307ef3bb5a1bf9b3ff995e41fc332834e6d42
Gwennael Gate <gwennaelgate@gmail.com>
2015-06-05 22:26:34 +0200

    fixing age and gender estimation service name

--- agegenderestimation/agegenderestimation.py
+++ agegenderestimation/agegenderestimation.py
@@ -3,6 +3,6 @@
 import angus
 
 conn = angus.connect()
-service = conn.services.get_service('agegender_estimation', version=1)
+service = conn.services.get_service('age_and_gender_estimation', version=1)
 job = service.process({'image': open('./macgyver.jpg')})
-print job.result['faces']
+print job.result
