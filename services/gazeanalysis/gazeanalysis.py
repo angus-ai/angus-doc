c8ea5ca7c643b367a19ae9532f10bf9634aca10b
Aureliren Moreau <aurelien.moreau@angus.ai>
2016-11-30 00:44:25 +0100

    Improve python example code for gaze service.

--- services/gazeanalysis/gazeanalysis.py
+++ services/gazeanalysis/gazeanalysis.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python
-
 import angus
+from pprint import pprint
 
 conn = angus.connect()
 service = conn.services.get_service('gaze_analysis', version=1)
 job = service.process({'image': open('./macgyver.jpg')})
-print job.result
+
+pprint(job.result)
--- services/gazeanalysis/gazeanalysis_fromwebcam.py
+++ services/gazeanalysis/gazeanalysis_fromwebcam.py
@@ -1,79 +1,70 @@
-#!/usr/bin/env python
 import StringIO
-
-import sys
 import math as m
 import cv2
-
 import numpy as np
 import angus
 
-if __name__ == '__main__':
-    
-    ### To grab the host computer web cam instead of a given file, try:
-    ### cap = cv2.VideoCapture(0)
-    cap = cv2.VideoCapture(0)
-    cap.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 640);
-    cap.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 480);
+def main(stream_index):
+    camera = cv2.VideoCapture(0)
+    camera.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 640);
+    camera.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 480);
+    camera.set(cv2.cv.CV_CAP_PROP_FPS, 10)
 
-    print "Input stream is of resolution: " + str(cap.get(3)) + " x " + str(cap.get(4))
+    if not camera.isOpened():
+        print("Cannot open stream of index {}".format(stream_index))
+        exit(1)
+
+    print("Input stream is of resolution: {} x {}".format(camera.get(3), camera.get(4)))
 
     conn = angus.connect()
     service = conn.services.get_service('gaze_analysis', 1)
     service.enable_session()
-    
-    while(cap.isOpened()):
-        ret, frame = cap.read()
-        if(frame != None):
-            ### angus.ai computer vision services require gray images right now.
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            ret, buff = cv2.imencode(".png", gray)
-            buff = StringIO.StringIO(np.array(buff).tostring())
 
-            job = service.process({"image": buff})
-            res = job.result
+    while camera.isOpened():
+        ret, frame = camera.read()
+        if not ret:
+            break
 
-            print "---------- Raw answer from Angus.ai -----------" 
-            print res
-            print "-----------------------------------------------" 
+        ### angus.ai computer vision services require gray images right now.
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        ret, buff = cv2.imencode(".jpg", gray, [cv2.IMWRITE_JPEG_QUALITY, 80])
+        buff = StringIO.StringIO(np.array(buff).tostring())
 
-            
-            if res['nb_faces'] > 0:
-                for i in range(0,res['nb_faces']):
-                    roi = res['faces'][i]['roi']
-                    cv2.rectangle(frame, (int(roi[0]), int(roi[1])), 
-                                         (int(roi[0] + roi[2]), int(roi[1] + roi[3])),
-                                         (0,255,0))
+        job = service.process({"image": buff})
+        res = job.result
 
-                    roll = res['faces'][i]['head_roll']
-                    yaw = res['faces'][i]['head_yaw']
-                    pitch = res['faces'][i]['head_pitch']
-                    
-                    center = (int(roi[0] + 0.5*roi[2]), int(roi[1] + 0.5*roi[3]))
-                        
-                    phi = pitch
-                    psi = roll
-                    theta = - yaw
-                    
-                    length = 150
-                    ### See here for details : https://en.wikipedia.org/wiki/Rotation_formalisms_in_three_dimensions
-                    xvec = length*(m.sin(phi)*m.sin(psi) - m.cos(phi)*m.sin(theta)*m.cos(psi))
-                    yvec = - length*(m.sin(phi)*m.cos(psi) - m.cos(phi)*m.sin(theta)*m.sin(psi))
-                    vec = (int(xvec), int(yvec))
+        for face in res['faces']:
+            x, y, dx, dy = map(int, face['roi'])
+            cv2.rectangle(frame, (x, y), (x+dx, y+dy), (0,255,0))
 
-                    cv2.line(frame, center, (center[0]+vec[0], center[1]+vec[1]), (255, 0, 0), 3)
-            
+            psi = face['head_roll']
+            theta = - face['head_yaw']
+            phi = face['head_pitch']
 
-            cv2.imshow('original', frame)
+            cx = int(x + 0.5*dx)
+            cy = int(y + 0.5*dy)
 
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-        else:
-            print "None image read"
+            length = 150
+            ### See here for details : https://en.wikipedia.org/wiki/Rotation_formalisms_in_three_dimensions
+            xvec = int(length*(m.sin(phi)*m.sin(psi) - m.cos(phi)*m.sin(theta)*m.cos(psi)))
+            yvec = int(- length*(m.sin(phi)*m.cos(psi) - m.cos(phi)*m.sin(theta)*m.sin(psi)))
 
+            cv2.line(frame, (cx, cy), (cx+xvec, cy+yvec), (255, 0, 0), 3)
+
+
+        cv2.imshow('original', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
     ### Disabling session on the server
     service.disable_session()
 
-    cap.release()
+    camera.release()
     cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    ### Web cam index might be different from 0 on your setup.
+    ### To grab a given video file instead of the host computer cam, try:
+    ### main("/path/to/myvideo.avi")
+    main(0)
