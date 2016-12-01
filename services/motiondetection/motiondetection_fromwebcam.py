8a710e0fd454f41942bbf2271092b66fd7a5a6e7
Aurelien Moreau <aurelien.moreau@angus.ai>
2016-12-01 10:12:29 +0100

    Improve python example code for motion detection service.

--- services/motiondetection/motiondetection_fromwebcam.py
+++ services/motiondetection/motiondetection_fromwebcam.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 import StringIO
 
 import angus
@@ -7,43 +6,43 @@ import numpy as np
 
 if __name__ == '__main__':
     ### Retrieve web cam video stream
-    cap = cv2.VideoCapture(0)
+    camera = cv2.VideoCapture(0)
 
-    if not cap.isOpened():
-        print "Cannot open webcam \"0\" stream."
+    if not camera.isOpened():
+        print("Cannot open stream of index {}".format(stream_index))
+        exit(1)
 
-    print "Video stream is of resolution " + str(cap.get(3)) + " x " + str(cap.get(4))
+    print("Input stream is of resolution: {} x {}".format(camera.get(3), camera.get(4)))
 
     conn = angus.connect()
     service = conn.services.get_service("motion_detection", 1)
 
     service.enable_session()
 
-    while(cap.isOpened()):
-        ret, frame = cap.read()
-        if not frame == None:
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            ret, buff = cv2.imencode(".png", gray)
-            buff = StringIO.StringIO(np.array(buff).tostring())
+    while camera.isOpened():
+        ret, frame = camera.read()
+        if not ret:
+            break
 
-            job = service.process({"image": buff})
-            res = job.result
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        ret, buff = cv2.imencode(".jpg", gray, [cv2.IMWRITE_JPEG_QUALITY, 80])
+        buff = StringIO.StringIO(np.array(buff).tostring())
 
-            if res['nb_targets'] > 0:
-                mp = res['targets'][0]['mean_position']
-                v = res['targets'][0]['mean_velocity']
-                cv2.circle(frame, (int(mp[0]), int(mp[1])), 5, (255,255,255))
-                cv2.line(frame,
-                         (int(mp[0]), int(mp[1])),
-                         (int(mp[0]) + int(v[0]), int(mp[1]) + int(v[1])),
-                         (255,255,255))
+        job = service.process({"image": buff})
+        res = job.result
 
+        for target in res['targets']:
+            x, y = target['mean_position']
+            vx, vy = target['mean_velocity']
 
-            cv2.imshow('original', frame)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
+            cv2.circle(frame, (x, y), 5, (255,255,255))
+            cv2.line(frame, (x, y), (x + vx, y + vy), (255,255,255))
+
+        cv2.imshow('original', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
     service.disable_session()
 
-    cap.release()
+    camera.release()
     cv2.destroyAllWindows()
