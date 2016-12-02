af685ffbfe2561df4e3908709321c7d7e9cef4f1
Aurelien Moreau <aurelien.moreau@angus.ai>
2016-12-02 15:26:06 +0100

    Improve python example code for upper body service.

--- services/upperbodydetection/upperbodydetection_fromwebcam.py
+++ services/upperbodydetection/upperbodydetection_fromwebcam.py
@@ -1,50 +1,54 @@
-#!/usr/bin/env python
 import StringIO
 import cv2
-import angus
 import numpy as np
-import pprint
+from pprint import pprint
+import angus
 
-if __name__ == '__main__':
-    ### Web cam index might be different from 0 on your setup.
-    stream_index = 0
-    cap = cv2.VideoCapture(stream_index)
-    cap.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 640)
-    cap.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 480)
+def main(stream_index):
+    camera = cv2.VideoCapture(stream_index)
+    camera.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 640)
+    camera.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 480)
+    camera.set(cv2.cv.CV_CAP_PROP_FPS, 10)
 
-    if not cap.isOpened():
-        print "Cannot open stream of index " + str(stream_index)
+    if not camera.isOpened():
+        print("Cannot open stream of index {}".format(stream_index))
         exit(1)
 
-    print "Video stream is of resolution " + str(cap.get(3)) + " x " + str(cap.get(4))
+    print("Input stream is of resolution: {} x {}".format(camera.get(3), camera.get(4)))
 
     conn = angus.connect()
     service = conn.services.get_service("upper_body_detection", version=1)
     service.enable_session()
 
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if not frame == None:
+    while camera.isOpened():
+        ret, frame = camera.read()
+        if not ret:
+            break
 
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            ret, buff = cv2.imencode(".png", gray)
-            buff = StringIO.StringIO(np.array(buff).tostring())
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        ret, buff = cv2.imencode(".jpg", gray, [cv2.IMWRITE_JPEG_QUALITY, 80])
 
-            job = service.process({"image": buff})
-            res = job.result
-            pprint.pprint(res)
+        buff = StringIO.StringIO(np.array(buff).tostring())
 
-            for bod in res['upper_bodies']:
-                roi = bod['upper_body_roi']
-                cv2.rectangle(frame, (int(roi[0]), int(roi[1])),
-                                     (int(roi[0] + roi[2]), int(roi[1] + roi[3])),
-                                     (0,255,0))
+        job = service.process({"image": buff})
+        res = job.result
+        pprint(res)
 
-            cv2.imshow('original', frame)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
+        for body in res['upper_bodies']:
+            x, y, dx, dy = body['upper_body_roi']
+            cv2.rectangle(frame, (x, y), (x+dx, y+dy), (0,255,0))
+
+        cv2.imshow('original', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
     service.disable_session()
 
-    cap.release()
+    camera.release()
     cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    ### Web cam index might be different from 0 on your setup.
+    ### To grab a given video file instead of the host computer cam, try:
+    ### main("/path/to/myvideo.avi")
+    main(0)
