27df64adaaab636bfaa062d78abf181f16562c3c
Raphael Lumbroso <raphael.lumbroso@angus.ai>
2018-02-21 12:29:37 +0100

    Update date in footer to 2018

--- conf.py
+++ conf.py
@@ -49,7 +49,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'angus.ai'
-copyright = u'2015-2017, Gwennael Gate, Aurelien Moreau'
+copyright = u'2015-2018, Gwennael Gate, Aurelien Moreau'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
